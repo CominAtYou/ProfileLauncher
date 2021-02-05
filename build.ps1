692f311be39f7dc402c71b3504e85ee11a2d52d1
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2021-02-05 13:21:34 -0600

    Dependency installation for build is now in script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,43 @@
+function checkSuccess([bool]$success) {
+    if ($success -eq $false) {
+        exit
+    }
+}
+function compileTS {
+    Write-Output "Compiling TypeScript..."
+    tsc
+    checkSuccess($?)
+}
+
+Get-Command npm 2>&1 > $NULL
+Write-Host "Checking for dependencies..."
+if ($? -eq $false) {
+    Write-Error "Node.js is required to run this script. Please install it then re-run the script."
+    exit
+}
+
+Get-Command tsc 2>&1 > $NULL
+if ($? -eq $false) {
+    Write-Output "tsc isn't detected, installing it for you now..."
+    npm i typescript -g 2>&1 > $NULL
+    if ($? -eq $false) {
+        Write-Output "Failed to install tsc! You may need to run this script as root if you are on Linux or macOS."
+        exit
+    }
+    Write-Output "Installed tsc!"
+}
+
+Get-Command pkg 2>&1 > $NULL
+if ($? -eq $false) {
+    Write-Output "pkg does not seem to be present, installing it for you now..."
+    npm i pkg -g 2>&1 > $NULL
+    if ($? -eq $false) {
+        Write-Output "Failed to install pkg! You may need to run this script as root if you are on Linux or macOS."
+        exit
+    }
+    Write-Output "Installed pkg!"
+}
+
 Write-Output "Available Targets:`n"
 Write-Output "1: Windows 64-bit"
 Write-Output "2: Windows 32-bit"
@@ -10,58 +50,46 @@ $buildSelection = Read-Host "Selection"
 $array = $buildSelection.ToLower().Split(", ")
 
 if ($array.Contains('1')) {
-    "Compiling TypeScript..."
-    tsc
-    if ($? -eq $false) {
-        exit
-    }
+    compileTS
     Write-Host "Building for Windows 64-bit..."
     pkg .\index.js --target node14-win-x64 --output build/profilelauncher-win-x64 > $NULL
+    checkSuccess($?)
     Write-Output "Build complete!"
 }
 if ($array.Contains('2')) {
-    "Compiling TypeScript..."
-    tsc
-    if ($? -eq $false) {
-        exit
-    }
+    compileTS
     Write-Host "Building for Windows 32-bit..."
     pkg .\index.js --target node14-win-x86 --output build/profilelauncher-win-x86 > $NULL
+    checkSuccess($?)
     Write-Output "Build complete!"
 }
 if ($array.Contains('3')) {
-    "Compiling TypeScript..."
-    tsc
-    if ($? -eq $false) {
-        exit
-    }
+    compileTS
     Write-Host "Building for macOS (Intel)..."
     pkg .\index.js --target node14-darwin-x64 --output build/profilelauncher-mac-x64 > $NULL
+    checkSuccess($?)
     Write-Output "Build complete!"
 }
 if ($array.Contains('4')) {
-    "Compiling TypeScript..."
-    tsc
-    if ($? -eq $false) {
-        exit
-    }
+    compileTS
     Write-Host "Building for Linux 64-bit..."
     pkg .\index.js --target node14-linux-x64 --output build/profilelauncher-linux-x64 > $NULL
+    checkSuccess($?)
     Write-Output "Build complete!"
 }
 if ($array.Contains('all')) {
-    "Compiling TypeScript..."
-    tsc
-    if ($? -eq $false) {
-        exit
-    }
+    compileTS
     Write-Host "Building for Windows 64-bit..."
     pkg .\index.js --target node14-win-x64 --output build/profilelauncher-win-x64 > $NULL
+    checkSuccess($?)
     Write-Host "Building for Windows 32-bit..."
     pkg .\index.js --target node14-win-x86 --output build/profilelauncher-win-x86 > $NULL
+    checkSuccess($?)
     Write-Host "Building for macOS (Intel)..."
     pkg .\index.js --target node14-darwin-x64 --output build/profilelauncher-mac-x64 > $NULL
+    checkSuccess($?)
     Write-Host "Building for Linux 64-bit..."
     pkg .\index.js --target node14-linux-x64 --output build/profilelauncher-linux-x64 > $NULL
+    checkSuccess($?)
     Write-Output "Build complete!"
 }
